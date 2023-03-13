ff8103cbf278dfbf36af2eeb620db9f4e84974df
supreethavadhani <supreethavadhani@gmail.com>
2023-03-12 23:58:57 -0700

    completed testing generator

--- src/main/java/in/cognitron/lms/Gen.java
+++ src/main/java/in/cognitron/lms/Gen.java
@@ -16,21 +16,9 @@ public class Gen {
 	private static final String SPEC_ROOT = "/Users/supreethavadhani/workspace/metadev-server-example/assets/spec/";
 	private static final String JAVA_ROOT = "/Users/supreethavadhani/workspace/metadev-server-example/src/main/java/";
 	private static final String TS_ROOT = "/Users/supreethavadhani/workspace/metadev-client-example/src/app/framework-modules/formdata/gen/";
+	private static final String TEMPLATE_ROOT = "/Users/supreethavadhani/workspace/metadev-client-example/src/app/framework-modules/formdata/template/";
+	private static final String Page_ROOT = "/Users/supreethavadhani/workspace/metadev-client-example/src/app/pages/";
 	private static final String TS_FORM_IMPORT_PREFIX = "../form/";
-
-//	private static final String SPEC_ROOT = "/Users/cognitrontechnologies/workspace/ws/banyantree-server/assets/spec/";
-//	private static final String JAVA_ROOT = "/Users/cognitrontechnologies/workspace/ws/banyantree-server/src/main/java/";
-//	private static final String TS_ROOT = "/Users/cognitrontechnologies/workspace/ws/banyantreeclient/src/app/framework-modules/formdata/gen/";
-
-	/**
-	 * Bhandi's Workspace
-	 */
-	// private static final String SPEC_ROOT =
-	// "c:/repos/bt-server/assets/spec/";
-	// private static final String JAVA_ROOT =
-	// "c:/repos/bt-server/src/main/java/";
-	// private static final String TS_ROOT =
-	// "c:/repos/bt-client/src/app/framework-modules/formdata/gen/";
 
 	/**
 	 *
@@ -39,9 +27,10 @@ public class Gen {
 	public static void main(final String[] args) {
 		final long start = System.currentTimeMillis();
 		if (args.length == 0) {
-			Generator.generate(SPEC_ROOT, JAVA_ROOT, JAVA_PACKAGE, TS_ROOT, TS_FORM_IMPORT_PREFIX);
-		} else if (args.length == 5) {
-			Generator.generate(args[0], args[1], args[2], args[3], args[4]);
+			Generator.generate(SPEC_ROOT, JAVA_ROOT, JAVA_PACKAGE, TS_ROOT, TS_FORM_IMPORT_PREFIX, TEMPLATE_ROOT,
+					Page_ROOT);
+		} else if (args.length == 6) {
+			Generator.generate(args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
 		} else {
 			System.err.print("Usage: Gen spec_root java_root java_package_name ts_root ts_form_import_prefix ");
 		}
