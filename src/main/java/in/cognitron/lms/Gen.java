5d5efe5652d1d21807d78bf6e2bc3830b389f435
supreethavadhani <supreethavadhani@gmail.com>
2023-03-14 23:42:13 -0700

    route generator complete

--- src/main/java/in/cognitron/lms/Gen.java
+++ src/main/java/in/cognitron/lms/Gen.java
@@ -18,6 +18,7 @@ public class Gen {
 	private static final String TS_ROOT = "/Users/supreethavadhani/workspace/metadev-client-example/src/app/framework-modules/formdata/gen/";
 	private static final String TEMPLATE_ROOT = "/Users/supreethavadhani/workspace/metadev-client-example/src/app/framework-modules/formdata/template/";
 	private static final String Page_ROOT = "/Users/supreethavadhani/workspace/metadev-client-example/src/app/pages/";
+	private static final String ROUTE_ROOT = "/Users/supreethavadhani/workspace/metadev-client-example/src/app/";
 	private static final String TS_FORM_IMPORT_PREFIX = "../form/";
 
 	/**
@@ -28,9 +29,9 @@ public class Gen {
 		final long start = System.currentTimeMillis();
 		if (args.length == 0) {
 			Generator.generate(SPEC_ROOT, JAVA_ROOT, JAVA_PACKAGE, TS_ROOT, TS_FORM_IMPORT_PREFIX, TEMPLATE_ROOT,
-					Page_ROOT);
+					Page_ROOT, ROUTE_ROOT);
 		} else if (args.length == 6) {
-			Generator.generate(args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
+			Generator.generate(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
 		} else {
 			System.err.print("Usage: Gen spec_root java_root java_package_name ts_root ts_form_import_prefix ");
 		}
